aade81dec7f1b92229b287add67b49b8a0563589
Zats1954 <alex.zacepin@gmail.com>
2024-08-09 10:55:36 +0200

    init commit

--- app/src/main/java/ru/zatsoft/pdd/MainActivity.kt
+++ app/src/main/java/ru/zatsoft/pdd/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import ru.zatsoft.pdd.databinding.ActivityMainBinding
 
-
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     val pdd = """26.1. Не позднее 4 часов 30 минут времени управления с момента завершения        
@@ -13,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 не менее 45 минут, после которого этим водителем может быть начат очередной период
 управления транспортным средством. Указанный перерыв для отдыха может быть разделен на 2 части или более,
 первая из которых должна составлять не менее 15 минут, а последняя - не менее 30 минут."""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
             binding.tvInfo.textSize = 14.0f
             pdd
         } else {
-            binding.tvInfo.textSize = 26.0f
+            binding.tvInfo.textSize = 24.0f
             "Информация"
         })}
     }
